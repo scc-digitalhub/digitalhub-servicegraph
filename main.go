b48b1442d083d9b865c790228f02e0b7f510d199
kazhamiakin <kazhamiakin@gmail.com>
2025-11-10 18:27:01 +0100

    More tests and cleanup

--- main.go
+++ main.go
@@ -5,21 +5,22 @@ import (
 
 	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/app"
 	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/model"
+
 	_ "github.com/scc-digitalhub/digitalhub-servicegraph/pkg/nodes/httpclient"
 	_ "github.com/scc-digitalhub/digitalhub-servicegraph/pkg/nodes/wsclient"
-	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/sinks/base"
-	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/sources"
-	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/sources/http"
-	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/sources/websocket"
-	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/streams"
-	"github.com/scc-digitalhub/digitalhub-servicegraph/pkg/streams/flow"
+	_ "github.com/scc-digitalhub/digitalhub-servicegraph/pkg/sinks/base"
+
+	_ "github.com/scc-digitalhub/digitalhub-servicegraph/pkg/sources/http"
+	_ "github.com/scc-digitalhub/digitalhub-servicegraph/pkg/sources/websocket"
 )
 
 func main() {
-	// mainWebSocketSync()
-	// mainWebSocketAsync()
+
 	// simpleYaml("test/simple-http-sync.yaml")
-	simpleYaml("test/simple-http-async.yaml")
+	// simpleYaml("test/simple-http-async.yaml")
+	// simpleYaml("test/simple-ws-sync.yaml")
+	simpleYaml("test/simple-ws-async.yaml")
+
 }
 
 func simpleYaml(path string) {
@@ -37,34 +38,4 @@ func simpleYaml(path string) {
 	}
 	app := app.NewApp(*graph)
 	app.Run()
-}
-
-func mainHttpAsync() {
-	http.NewHTTPSource(http.NewConfiguration(8080, 0, 0, 0, 100000)).StartAsync(&TestFactory{}, base.NewStdoutSink())
-}
-
-func mainWebSocketSync() {
-	websocket.NewWSSource(websocket.NewConfiguration(8080, 2)).Start(&TestFactory{})
-}
-
-func mainWebSocketAsync() {
-	websocket.NewWSSource(websocket.NewConfiguration(8080, 2)).StartAsync(&TestFactory{}, base.NewStdoutSink())
-}
-
-type TestFactory struct {
-	sources.FlowFactory
-}
-
-func (f *TestFactory) GenerateFlow(source streams.Source, sink streams.Sink) {
-	source.Via(flow.NewMap(mock, 1).Via(flow.NewMap(mock, 1))).To(sink)
-}
-
-func mock(input interface{}) interface{} {
-	switch v := input.(type) {
-	case string:
-		return v + " - processed"
-	case streams.Event:
-		return string(v.GetBody()) + " - processed"
-	}
-	return input
 }
