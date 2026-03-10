523eb0b29592c359abe64c3270e02ebf18e5a27c
kazhamiakin <kazhamiakin@gmail.com>
2026-03-10 13:10:33 +0100

    version fix for tests to pass

--- pkg/util/otel.go
+++ pkg/util/otel.go
@@ -16,7 +16,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
